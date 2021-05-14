4048824bbaf0970de699e9eca79d5a078e9409f2
dyerai <dyerai734@yahoo.com>
2021-05-14 13:46:20 -0400

    created skeleton of MapRequisites.py

---
+++ src/utils/MapRequisites.py
@@ -0,0 +1,22 @@
+import re
+
+"""
+Cases in requisite string:
+    () and () or x
+    () or x
+    () and () or x
+    n,n,n,n or n or x
+    x and () or
+    n or n, x
+    None
+    n or x
+    Consent
+    n
+    n. Not open for students with credit for n or n
+    class standing
+"""
+def mapRequisites(courses):
+    pass
+
+def __parseRequisite():
+    pass
